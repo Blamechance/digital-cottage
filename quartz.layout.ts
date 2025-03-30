4717e6af64c772336f171a4a72b48e041b961c46
Tommy Cao <vtommycao@gmail.com>
2025-03-30 22:25:21 +1100

    minor change: remove topTags it would not work on pages other than the index.html/md page anyway. commenting out for now and keeping AllTags.

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -33,7 +33,7 @@ export const defaultContentPageLayout: PageLayout = {
   right: [
     Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
-    Component.DesktopOnly(Component.TopTags()), 
+    //Component.DesktopOnly(Component.TopTags()), 
     Component.DesktopOnly(Component.AllTags()),  
     Component.Backlinks(),
   ],
